42d8d36c1623d0fa0fee9a7066b61aa06a3a563f
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-03 14:23:37 +0300

    before custom hook
    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import SideBar from "./components/SideBar";
+import { ReactNotifications } from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
 
 function App() {
-  return <SideBar />;
+  return (
+    <>
+      <ReactNotifications />
+      <SideBar />
+    </>
+  );
 }
 
 export default App;
--- src/components/pages/today/TodayTable.tsx
+++ src/components/pages/today/TodayTable.tsx
@@ -2,8 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import "./todayStyles.css";
-import Axios from "axios";
+import Axios, { CancelTokenSource } from "axios";
 import domain from "../../../domain";
+import { iNotification, Store } from "react-notifications-component";
+const defaultSettings: iNotification = {
+  container: "bottom-center",
+  animationIn: ["animate__animated", "animate__fadeIn"],
+  animationOut: ["animate__animated", "animate__fadeOut"],
+  dismiss: {
+    duration: 3000,
+    onScreen: true,
+  },
+  insert: "top",
+};
 
 export default function TodayTable() {
   const [all, setAll] = useState([
@@ -15,11 +26,65 @@ export default function TodayTable() {
       actions: "loading...",
     },
   ]);
+  let isFetching = false;
+
+  const getAll = async (source: CancelTokenSource) => {
+    isFetching = true;
+    Store.removeAllNotifications();
+    Store.addNotification({
+      title: "Connecting to Server",
+      message: "Trying for 3 seconds to fetch data",
+      type: "info",
+
+      ...defaultSettings,
+    });
+    const promise = Axios.get(domain + "all", { cancelToken: source.token });
+    promise.then((res) => {
+      setAll([...res.data.events, ...res.data.tasks]);
+      Store.removeAllNotifications();
+      Store.addNotification({
+        title: "Success",
+        message: "Data was fetched from the server",
+        type: "success",
+
+        ...defaultSettings,
+      });
+      isFetching = false;
+    });
+    promise.catch((err) => {
+      if (err.message !== "canceled") {
+        Store.removeAllNotifications();
+        Store.addNotification({
+          title: "Error",
+          message: err.message,
+          type: "danger",
+
+          ...defaultSettings,
+        });
+      }
+      isFetching = false;
+    });
+  };
 
   useEffect(() => {
-    const promise = Axios.get(domain + "all");
-    promise.then((res) => setAll(res.data));
-    return () => {};
+    const source = Axios.CancelToken.source();
+    getAll(source);
+    return () => {
+      setTimeout(() => {
+        if (isFetching) {
+          source.cancel();
+          Store.removeAllNotifications();
+          Store.addNotification({
+            title: "Error!",
+            message: "Connection to server Timeout",
+            type: "danger",
+
+            ...defaultSettings,
+          });
+          isFetching = false;
+        }
+      }, 3000);
+    };
   }, []);
 
   return (
@@ -37,8 +102,8 @@ export default function TodayTable() {
         <Tbody>
           {all &&
             all.length > 0 &&
-            all.map((item) => (
-              <Tr>
+            all.map((item, i) => (
+              <Tr key={i}>
                 <Td>{item.type}</Td>
                 <Td>{item.priority}</Td>
                 <Td>{item.title}</Td>
